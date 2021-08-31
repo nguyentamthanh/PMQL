14fbe24dd9349323ce56dd9c334571b462d6330d
nguyentamthanh <77631594+nguyentamthanh@users.noreply.github.com>
2021-08-31 07:57:48 +0700

    Add project files.

---
+++ bai1/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace bai1
+{
+    partial class txtso
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNhap = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNhap = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtTongle = new System.Windows.Forms.TextBox();
+            this.txtTongChan = new System.Windows.Forms.TextBox();
+            this.txtTong = new System.Windows.Forms.TextBox();
+            this.txtDaySo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtNhap);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(497, 125);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "nhập số";
+            // 
+            // txtNhap
+            // 
+            this.txtNhap.Location = new System.Drawing.Point(85, 34);
+            this.txtNhap.Name = "txtNhap";
+            this.txtNhap.Size = new System.Drawing.Size(368, 27);
+            this.txtNhap.TabIndex = 1;
+            this.txtNhap.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtNhap_MouseClick);
+            this.txtNhap.MouseEnter += new System.EventHandler(this.txtNhap_MouseEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "nhập số";
+            // 
+            // btnNhap
+            // 
+            this.btnNhap.Location = new System.Drawing.Point(195, 143);
+            this.btnNhap.Name = "btnNhap";
+            this.btnNhap.Size = new System.Drawing.Size(94, 29);
+            this.btnNhap.TabIndex = 2;
+            this.btnNhap.Text = "Nhập";
+            this.btnNhap.UseVisualStyleBackColor = true;
+            this.btnNhap.Click += new System.EventHandler(this.btnNhap_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtTongle);
+            this.groupBox2.Controls.Add(this.txtTongChan);
+            this.groupBox2.Controls.Add(this.txtTong);
+            this.groupBox2.Controls.Add(this.txtDaySo);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 200);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(497, 189);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "kết quả";
+            // 
+            // txtTongle
+            // 
+            this.txtTongle.Location = new System.Drawing.Point(126, 131);
+            this.txtTongle.Name = "txtTongle";
+            this.txtTongle.Size = new System.Drawing.Size(327, 27);
+            this.txtTongle.TabIndex = 7;
+            // 
+            // txtTongChan
+            // 
+            this.txtTongChan.Location = new System.Drawing.Point(126, 99);
+            this.txtTongChan.Name = "txtTongChan";
+            this.txtTongChan.Size = new System.Drawing.Size(327, 27);
+            this.txtTongChan.TabIndex = 6;
+            // 
+            // txtTong
+            // 
+            this.txtTong.Location = new System.Drawing.Point(126, 63);
+            this.txtTong.Name = "txtTong";
+            this.txtTong.Size = new System.Drawing.Size(327, 27);
+            this.txtTong.TabIndex = 5;
+            // 
+            // txtDaySo
+            // 
+            this.txtDaySo.Location = new System.Drawing.Point(126, 26);
+            this.txtDaySo.Name = "txtDaySo";
+            this.txtDaySo.Size = new System.Drawing.Size(327, 27);
+            this.txtDaySo.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "tổng lẻ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "tổng chẵn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "tổng dãy số";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Dãy số vừa nhập ";
+            // 
+            // txtso
+            // 
+            this.AcceptButton = this.btnNhap;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnNhap);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "txtso";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNhap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNhap;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtTongle;
+        private System.Windows.Forms.TextBox txtTongChan;
+        private System.Windows.Forms.TextBox txtTong;
+        private System.Windows.Forms.TextBox txtDaySo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ bai1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai1
+{
+    public partial class txtso : Form
+    {
+        public txtso()
+        {
+            InitializeComponent();
+        }
+        int tong = 0,tongchan=0,tongle=0;
+
+        private void txtNhap_MouseEnter(object sender, EventArgs e)
+        {
+            txtNhap.SelectAll();
+        }
+
+        private void txtNhap_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtNhap.SelectAll();
+        }
+
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            txtDaySo.Text = txtDaySo.Text + txtNhap.Text + ",";
+            
+            int iNhap = Convert.ToInt32(txtNhap.Text);
+            tong = tong + iNhap;
+            txtTong.Text = tong.ToString();
+            if (iNhap%2==0)
+            {
+                
+                tongchan = tongchan + iNhap;
+              
+            }    
+            else
+            {
+                tongle = tongle + iNhap;
+            }
+            txtTongChan.Text = tongchan.ToString();
+            txtTongle.Text = tongle.ToString();
+            //tính ttoongr
+        }
+    }
+}
